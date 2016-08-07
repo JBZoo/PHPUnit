125d77cb34c76e3fbd3f3d1beafeec2bab6f0fa4
smetdenis <admin@jbzoo.com>
2016-08-08 03:31:37 +0700

    code style

--- bin/fake-index.php
+++ bin/fake-index.php
@@ -12,12 +12,10 @@
  * @link      https://github.com/JBZoo/PHPUnit
  */
 
+use JBZoo\Data\Data;
 use JBZoo\PHPUnit\CovCatcher;
-use JBZoo\Utils\Env;
 use JBZoo\Utils\Sys;
 use Ulrichsg\Getopt\Getopt;
-
-// $_SERVER['SCRIPT_NAME'] = '/index.php'; // #FUCK!!! https://bugs.php.net/bug.php?id=61286
 
 // To help the built-in PHP dev server, check if the request was actually for
 // something which should probably be served as a static file
@@ -36,7 +34,8 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-$vendorPaths = array(
+// Try to find and load composer autoloader
+$vendorPaths = [
     realpath(__DIR__ . '/vendor/autoload.php'),
     realpath(__DIR__ . '/../vendor/autoload.php'),
     realpath(__DIR__ . '/../../vendor/autoload.php'),
@@ -44,7 +43,7 @@ $vendorPaths = array(
     realpath(__DIR__ . '/../../../../vendor/autoload.php'),
     realpath(__DIR__ . '/../../../../../vendor/autoload.php'),
     realpath('./vendor/autoload.php'),
-);
+];
 
 foreach ($vendorPaths as $vendorPath) {
     if ($vendorPath && file_exists($vendorPath)) {
@@ -53,33 +52,33 @@ foreach ($vendorPaths as $vendorPath) {
     }
 }
 
-$cliOptions = new Getopt(array(
-    array(null, 'index', Getopt::OPTIONAL_ARGUMENT),
-    array(null, 'cov-src', Getopt::OPTIONAL_ARGUMENT),
-    array(null, 'cov-xml', Getopt::OPTIONAL_ARGUMENT),
-    array(null, 'cov-cov', Getopt::OPTIONAL_ARGUMENT),
-    array(null, 'cov-html', Getopt::OPTIONAL_ARGUMENT),
-));
+// Parse additional options
+$cliOptions = new Getopt([
+    [null, 'index', Getopt::OPTIONAL_ARGUMENT],
+    [null, 'cov-src', Getopt::OPTIONAL_ARGUMENT],
+    [null, 'cov-xml', Getopt::OPTIONAL_ARGUMENT],
+    [null, 'cov-cov', Getopt::OPTIONAL_ARGUMENT],
+    [null, 'cov-html', Getopt::OPTIONAL_ARGUMENT],
+]);
 
 $cliOptions->parse(getenv('PHPUNINT_ARGUMENTS'));
 
 $realIndex = isset($realIndex) ? $realIndex : realpath($cliOptions->getOption('index'));
 
-if (class_exists('\JBZoo\PHPUnit\CovCatcher') && !(Sys::isPhp7() && Env::hasXdebug())) {
-    $hash = md5(implode('||', array(
-        serialize($_REQUEST),
-        serialize($_SERVER),
-        PHP_VERSION
-    )));
+if (class_exists('\JBZoo\PHPUnit\CovCatcher') && !(Sys::isPHP7() && Sys::hasXdebug())) {
+    $testname = (new Data($_REQUEST))->get('testname');
 
-    $catcher = new CovCatcher($hash, array(
+    $coverHash = md5(implode('||', [serialize($_REQUEST), serialize($_SERVER), PHP_VERSION]));
+    $coverHash = $testname ? $testname . '-' . $coverHash : $testname;
+
+    $covCatcher = new CovCatcher($coverHash, [
         'src'  => $cliOptions->getOption('cov-src'),
         'xml'  => $cliOptions->getOption('cov-xml'),
         'cov'  => $cliOptions->getOption('cov-cov'),
         'html' => $cliOptions->getOption('cov-html'),
-    ));
+    ]);
 
-    $result = $catcher->includeFile($realIndex);
+    $result = $covCatcher->includeFile($realIndex);
 
 } else {
     $result = require_once $realIndex;
