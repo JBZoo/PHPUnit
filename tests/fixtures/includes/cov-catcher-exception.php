506e4c9332260cb6053b1a43829db9a5ebb4b376
Denis Smet <denis.smetannikov@yandex.ru>
2024-01-28 02:03:31 +0400

    Update PHP deps. PHP 8.3

--- tests/fixtures/includes/cov-catcher-exception.php
+++ tests/fixtures/includes/cov-catcher-exception.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
  */
 function showError(): void
 {
-    throw new \RuntimeException('Some exception');
+    throw new RuntimeException('Some exception');
 }
 
 echo 'Some text message';
